c6b7aef7f0ee3529a5f7da056a92ce916c5abfaf
Enrique Hernandez <enriquehernndez@Enriques-MacBook-Pro.local>
2017-10-26 19:44:21 -0700

    db implemented

--- server/index.js
+++ server/index.js
@@ -8,158 +8,35 @@ var faker = require('faker');
 var fs = require('fs');
 var location = require('./fakeLocationData.txt');
 
-//objectives:
-//  create fake data
-//  parse fake data into desired object
-//  store fake parsed data into db
+// todays objectives
+//set up and start loading database
+//start about elastic search, kibana, and AWS.
 
 
-faker.locale = 'en_US';
 
-var makeid = function() {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (var i = 0; i < 6; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
-
-var makeYelpId = function(name) {
-  return name.split(' ').join('-');
-};
-
-var randomPriceMaker = function() {
-  var result = '';
-  var randomNumber = Math.ceil(Math.random() * 4);
-  for (var i = 0; i < randomNumber; i++) {
-    result += '$';
-  }
-  return result;
-};
-
-var fakeLocationData = location.location;
-fakeLocationData = fakeLocationData.split('\t\t\n');
 // console.log(fakeLocationData[0].split('\tpumpkins\t'));
 // console.log(fakeLocationData.length);
 
 
 
 
-
 app.get('/', (req, res) => {
-  var randomAliasOne = faker.random.word();
-  var randomAliasTwo = faker.random.word();
-  var randomAliasThree = faker.random.word();
-  var name = faker.name.findName();
-  var rating = Math.floor(Math.random() * 6);
-  var randomLocationIndex = Math.floor(Math.random() * 81257);
-  var randomLocation = fakeLocationData[0].split('\tpumpkins\t');
-  // console.log(randomLocation);
+  // var yelp = fetcher.generateDetailedRestaurantsObject();
+  // var restaurantProfile = fetcher.makeRestaurantProfile(yelp);
+  // var restaurantProfileWithoutReviews = fetcher.makeRestaurantProfileWithoutReviews(yelp);
+  // console.log(typeof parseFloat(restaurantProfile.price), parseFloat(restaurantProfile.price), restaurantProfile.price);
+  // res.send(JSON.stringify(restaurantProfile.coordinates.latitude));
+
+  //make fake yelp response 
+  //make restaurant profile
+  //save each restaurant
+  //save each category
+  //save each restaurantCategory
+  //  save each user
+  //    save each review
+  //
 
 
-  var yelpObject = {
-    'id': makeYelpId(name), 
-    'name': name, 
-    'image_url': 'https://s3-media2.fl.yelpcdn.com/bphoto/SRj2lQJkjLQ-TxccWdw2eQ/o.jpg', 
-    'is_claimed': true, 
-    'is_closed': false, 
-    'url': 'https://www.yelp.com/biz/taqueria-casas-kissimmee?adjust_creative=p5JPLml8YiVq3TUr8ab8hw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=p5JPLml8YiVq3TUr8ab8hw', 
-    'phone': '+14073900703', 
-    'display_phone': '(407) 390-0703', 
-    'review_count': 37, 
-    'categories': [
-      {
-        'alias': randomAliasOne, 
-        'title': randomAliasOne
-      },
-      {
-        'alias': randomAliasTwo, 
-        'title': randomAliasTwo
-      },
-      {
-        'alias': randomAliasThree, 
-        'title': randomAliasThree
-      }
-    ], 
-    'rating': rating || 5, 
-    'location': {
-      'address1': '4384 W Vine St', 
-      'address2': '', 
-      'address3': '', 
-      'city': randomLocation[1], 
-      'zip_code': randomLocation[0], 
-      'country': randomLocation[5], 
-      'state': randomLocation[2], 
-      'display_address': [
-        '4384 W Vine St', 
-        'Kissimmee, FL 34746'
-      ], 
-      'cross_streets': ''
-    }, 
-    'coordinates': {
-      'latitude': randomLocation[3], 
-      'longitude': randomLocation[4]
-    }, 
-    'photos': [
-      'https://s3-media2.fl.yelpcdn.com/bphoto/SRj2lQJkjLQ-TxccWdw2eQ/o.jpg', 
-      'https://s3-media1.fl.yelpcdn.com/bphoto/80JBY2aGcTlBVRgP7CgcKA/o.jpg', 
-      'https://s3-media3.fl.yelpcdn.com/bphoto/hdLYkUVF2ZtPQ3aTnUBzbw/o.jpg'
-    ], 
-    'price': randomPriceMaker(), 
-    'hours': [
-      {
-        'open': [
-          {
-            'is_overnight': false, 
-            'start': '1000', 
-            'end': '2200', 
-            'day': 0
-          }, 
-          {
-            'is_overnight': false, 
-            'start': '1000', 
-            'end': '2200', 
-            'day': 1
-          }, 
-          {
-            'is_overnight': false, 
-            'start': '1000', 
-            'end': '2200', 
-            'day': 2}, 
-          {
-            'is_overnight': false,
-            'start': '1000', 
-            'end': '2200', 
-            'day': 3}, 
-          {
-            'is_overnight': false, 
-            'start': '1000', 
-            'end': '2200', 
-            'day': 4
-          }, 
-          {
-            'is_overnight': false, 
-            'start': '1000', 
-            'end': '2200', 
-            'day': 5
-          }, 
-          {
-            'is_overnight': false, 
-            'start': '1000', 
-            'end': '2200', 
-            'day': 6
-          }
-        ], 
-        'hours_type': 'REGULAR', 
-        'is_open_now': true
-      }
-    ], 
-    'transactions': []
-  };
-
-  console.log(yelpObject);
-  res.send(JSON.stringify(yelpObject));
 });
 
 
@@ -167,7 +44,7 @@ app.get('/', (req, res) => {
 
 
 
-
+//remember to store in database first then set id's on each object and finally send objects to teammates
 
 
 
